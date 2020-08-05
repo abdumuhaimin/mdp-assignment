9f533d4679099d068bc1166631db37baae0970bd
Abdu Muhaimin <abdumuhaimin.1@gmail.com>
2020-08-05 22:43:36 +0800

    Added toolbar for menu overflow

--- WeddingCostApp/app/src/main/java/com/unimy/abdumuhaimin/weddingcostapp/MainActivity.java
+++ WeddingCostApp/app/src/main/java/com/unimy/abdumuhaimin/weddingcostapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.unimy.abdumuhaimin.weddingcostapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
